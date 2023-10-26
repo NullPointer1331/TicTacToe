e1a5c9fb664603eab46a9bc6f81015eb59ef92e7
NullPointer1331 <130084942+NullPointer1331@users.noreply.github.com>
2023-10-26 14:04:52 -0700

    Made game fully functional

--- app/src/main/java/com/example/tictactoe/MainActivity.kt
+++ app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -25,6 +25,7 @@ class MainActivity : AppCompatActivity() {
         for(button in ticTacToeButtons) {
             button.setOnClickListener {
                 buttonClick(button, turnDisplay)
+                checkWin(ticTacToeButtons)
             }
         }
         val newGame = findViewById<Button>(R.id.NewGame)
@@ -47,5 +48,70 @@ class MainActivity : AppCompatActivity() {
         }
         xTurn = !xTurn
         button.isEnabled = false
+    }
+    fun checkWin(buttons: Array<Button>) {
+        // Check tie
+        var tie = true
+        for(button in buttons) {
+            if(button.text == "") {
+                tie = false
+                break
+            }
+        }
+        if(tie) {
+            val turnDisplay = findViewById<TextView>(R.id.TurnDisplay)
+            turnDisplay.text = "It's a tie!"
+            return
+        }
+        // Check vertical
+        for(i in 0..2) {
+            if(buttons[i].text == buttons[i+3].text && buttons[i].text == buttons[i+6].text) {
+                if(buttons[i].text == "X") {
+                    win("X", buttons)
+                    return
+                } else if(buttons[i].text == "O") {
+                    win("O", buttons)
+                    return
+                }
+            }
+        }
+        // Check horizontal
+        for (i in 0..6 step 3) {
+            if(buttons[i].text == buttons[i+1].text && buttons[i].text == buttons[i+2].text) {
+                if(buttons[i].text == "X") {
+                    win("X", buttons)
+                    return
+                } else if(buttons[i].text == "O") {
+                    win("O", buttons)
+                    return
+                }
+            }
+        }
+        // Check diagonal
+        if(buttons[0].text == buttons[4].text && buttons[0].text == buttons[8].text) {
+            if(buttons[0].text == "X") {
+                win("X", buttons)
+                return
+            } else if(buttons[0].text == "O") {
+                win("O", buttons)
+                return
+            }
+        }
+        if(buttons[2].text == buttons[4].text && buttons[2].text == buttons[6].text) {
+            if(buttons[2].text == "X") {
+                win("X", buttons)
+                return
+            } else if(buttons[2].text == "O") {
+                win("O", buttons)
+                return
+            }
+        }
+    }
+    fun win(winner: String, buttons: Array<Button>) {
+        val turnDisplay = findViewById<TextView>(R.id.TurnDisplay)
+        turnDisplay.text = "$winner wins!"
+        for(button in buttons) {
+            button.isEnabled = false
+        }
     }
 }
